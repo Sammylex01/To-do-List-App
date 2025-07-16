26a72c017520c62ae7bc23ebda85adbdf84095be
Samdroid <samueloyekoya12@gmail.com>
2025-07-16 07:28:24 +0100

    Update App.js
    Added logic to create and delete a task

--- App.js
+++ App.js
@@ -1,8 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from "react";
+import { Platform, StyleSheet, Text, View, TouchableOpacity, Keyboard } from 'react-native';
+import React, {useState} from "react";
 import Task from './components/Task';
+import { KeyboardAvoidingView, TextInput} from 'react-native-web';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+
   return (
     <View style={styles.container}>
       {/* Today's Tasks */}
@@ -11,13 +28,37 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go! */}
-          <Task text={'Task 1'}/>
-          <Task text={'Task 2'}/>  
+          {
+            taskItems.map((item, index) => {
+              return  (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+          {/* <Task text={'Task 1'}/>
+          <Task text={'Task 2'}/>   */}
         </View>
 
       </View>
 
+      {/* Write a task */}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "android" ? "height" : "padding"}
+      style={styles.writeTaskWrapper}
+    >
+      <TextInput style={styles.input} placeholder={'Write a task'} value = {task} onChangeText={text => setTask(text)}/>
+
+      <TouchableOpacity onPress={() => handleAddTask()}>
+        <View style={styles.addWrapper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
+      </TouchableOpacity>
+      
+    </KeyboardAvoidingView>
     </View>
+
   );
 }
 
@@ -38,4 +79,32 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#c0c0c0',
+    borderWidth: 1,
+    width: 250
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#c0c0c0',
+    borderWidth: 1
+  },
+  addText: {}
 });
